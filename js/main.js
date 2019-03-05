11dcc7afad97d0a0c7fec49ac1024a997d295bb5
chevyckj <chevyckj@gmail.com>
2019-03-05 21:07:09 +0800

    A

--- js/main.js
+++ js/main.js
@@ -231,7 +231,7 @@ function init()
 	});
 
 	let loader = new THREE.FontLoader();
-	loader.load('fonts/Alien Robot_Regular.json', function (font)
+	loader.load('fonts/Montserrat_Regular.json', function (font)
 	{
 		{
 			let geometry = new THREE.TextGeometry('X Reality Lab',
