4bfd1d82d7ea51599f2480787a7da1fe6d3c3af0
woshikj <chevyckj@gmail.com>
2019-03-05 11:23:10 +0800

    Added balls

--- js/main.js
+++ js/main.js
@@ -4,12 +4,44 @@ var arToolkitSource, arToolkitContext;
 
 var markerRoot1;
 
+var textList = [];
+var textAppeared = false;
+
+var shapeList = [];
+
+function SpawnShape(gotoCam)
+{
+	let geometry = new THREE.SphereGeometry(Math.random() * 0.1 + 0.05, 32, 32);
+	
+	let material = new THREE.MeshStandardMaterial({
+		color: Math.random() * 0x555555 + 0xaaaaaa,
+		roughness: 0.5,
+		metalness: 0.5
+	});
+	
+	let mesh = new THREE.Mesh( geometry, material );
+	mesh.position.x = markerRoot1.position.x + Math.random() * 2 - 1;
+	mesh.position.y = markerRoot1.position.y + Math.random() * 2 - 1;
+	mesh.position.z = markerRoot1.position.z;
+	mesh.velocity = {x:Math.random() * 2 - 1,y:Math.random() * 1.5 + 0.5,z:Math.random() * 0.9 + 0.1};
+	var length = Math.sqrt(mesh.velocity.x * mesh.velocity.x + mesh.velocity.y * mesh.velocity.y + mesh.velocity.z * mesh.velocity.z);
+	mesh.velocity.x /= length;
+	mesh.velocity.y /= length;
+	mesh.velocity.z /= length;
+	mesh.gotoCam = (gotoCam != null);
+	shapeList.push(mesh);
+	scene.add(mesh);
+}
+
 function init()
 {
     scene = new THREE.Scene();
 
     let ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
-    scene.add(ambientLight);
+	scene.add(ambientLight);
+	
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	scene.add( directionalLight );
 
     camera = new THREE.Camera();
     scene.add(camera);
@@ -81,31 +113,73 @@ function init()
 	scene.add(markerRoot1);
 	let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
 		type: 'pattern', patternUrl: "data/pattern-marker.patt",
-	})
+	});
+
+	markerControls1.addEventListener('markerFound', function()
+	{
+		if(!textAppeared)
+		{
+			textAppeared = true;
+			for(var i = 0; i < textList.length; ++i)
+			{
+				(function(id)
+				{
+					let scale = {x:0,y:0,z:0};
+					new TWEEN.Tween(scale)
+							.to({x:1.0, y:1.0, z:1.0}, 2000)
+							.easing(TWEEN.Easing.Elastic.Out)
+							.onUpdate(function()
+							{
+								textList[id].scale.x = scale.x;
+								textList[id].scale.y = scale.y;
+								textList[id].scale.z = scale.z;
+							})
+							.start();
+				})(i);
+			}
+
+			for(var i = 0; i < 20; ++i)
+			{
+			}
+		}
+		SpawnShape();
+	});
 
 	let loader = new THREE.FontLoader();
 	loader.load('fonts/Forte_Regular.json', function (font)
 	{
 		{
-			let geometry = new THREE.TextGeometry('Digital PlayLab',
+			let geometry = new THREE.TextGeometry('X Reality Lab',
 			{
 				font: font,
-				size: 0.5,
+				size: 0.75,
 				height: 0.3,
 				curveSegments:12
 			});
-			let material	= new THREE.MeshNormalMaterial({
+			/*let material	= new THREE.MeshNormalMaterial({
 				transparent: true,
 				opacity: 1.0,
 				side: THREE.DoubleSide
-			}); 
+			}); */
+			let material = new THREE.MeshStandardMaterial({
+				color: 0x42bff4,
+				roughness: 0.5,
+				metalness: 0.5
+			});
 			
 			let mesh = new THREE.Mesh( geometry, material );
-			mesh.position.z = -1.0;
-			mesh.position.x = -2.0;
+			mesh.position.z = -1.15;
+			mesh.position.x = -2.5;
 			mesh.rotation.x = Math.PI * 0.5;
 			mesh.rotation.y = Math.PI;
 			mesh.rotation.z = Math.PI;
+			mesh.scale.x = 0;
+			mesh.scale.y = 0;
+			mesh.scale.z = 0;
+
+			directionalLight.target = mesh;
+
+			textList.push(mesh);
 			
 			markerRoot1.add( mesh );
 		}
@@ -117,18 +191,25 @@ function init()
 				height: 0.3,
 				curveSegments:12
 			});
-			let material	= new THREE.MeshNormalMaterial({
+			/*let material	= new THREE.MeshNormalMaterial({
 				transparent: true,
 				opacity: 1.0,
 				side: THREE.DoubleSide
-			}); 
+			}); */
+			let material = new THREE.MeshStandardMaterial({
+				color: 0xffffff,
+				roughness: 0.5,
+				metalness: 0.5
+			});
 			
 			let mesh = new THREE.Mesh( geometry, material );
 			mesh.position.z = 0;
-			mesh.position.x = -1.65;
+			mesh.position.x = -1.85;
 			mesh.rotation.x = Math.PI * 0.5;
 			mesh.rotation.y = Math.PI;
 			mesh.rotation.z = Math.PI;
+
+			textList.push(mesh);
 			
 			markerRoot1.add( mesh );
 		}
@@ -140,18 +221,25 @@ function init()
 				height: 0.3,
 				curveSegments:12
 			});
-			let material	= new THREE.MeshNormalMaterial({
+			/*let material	= new THREE.MeshNormalMaterial({
 				transparent: true,
 				opacity: 1.0,
 				side: THREE.DoubleSide
-			}); 
+			}); */
+			let material = new THREE.MeshStandardMaterial({
+				color: 0x42bff4,
+				roughness: 0.5,
+				metalness: 0.5
+			});
 			
 			let mesh = new THREE.Mesh( geometry, material );
 			mesh.position.z = 1;
-			mesh.position.x = -1.65;
+			mesh.position.x = -1.85;
 			mesh.rotation.x = Math.PI * 0.5;
 			mesh.rotation.y = Math.PI;
 			mesh.rotation.z = Math.PI;
+
+			textList.push(mesh);
 			
 			markerRoot1.add( mesh );
 		}
@@ -178,6 +266,38 @@ function update()
 	// update artoolkit on every frame
 	if ( arToolkitSource.ready !== false )
 		arToolkitContext.update( arToolkitSource.domElement );
+
+	for(var i = 0; i < shapeList.length; ++i)
+	{
+		if(shapeList[i].position.y < -10)
+		{
+			scene.remove(shapeList[i]);
+			shapeList[i].geometry.dispose();
+			shapeList[i].material.dispose();
+			shapeList.splice(i,1);
+			--i;
+			continue;
+		}
+		if(shapeList[i].gotoCam)
+		{
+			var dx = camera.position.x - shapeList[i].position.x;
+			var dy = camera.position.y - shapeList[i].position.y;
+			var dz = camera.position.z - shapeList[i].position.z;
+			var length = Math.sqrt(dx*dx + dy*dy + dz*dz);
+			dx /= length;
+			dy /= length;
+			dz /= length;
+
+			shapeList[i].position.x = shapeList[i].position.x + dx * 0.5;
+			shapeList[i].position.y = shapeList[i].position.y + dy * 0.5;
+			shapeList[i].position.z = shapeList[i].position.z + dz * 0.5;
+			continue;
+		}
+		shapeList[i].position.x = shapeList[i].position.x + shapeList[i].velocity.x * 0.5;
+		shapeList[i].position.y = shapeList[i].position.y + shapeList[i].velocity.y * 0.5;
+		shapeList[i].position.z = shapeList[i].position.z + shapeList[i].velocity.z * 0.5;
+		shapeList[i].velocity.y -= 0.981 * 0.1;
+	}
 }
 
 
@@ -193,6 +313,7 @@ function animate()
 	deltaTime = clock.getDelta();
 	totalTime += deltaTime;
 	update();
+	TWEEN.update();
 	render();
 }
 
