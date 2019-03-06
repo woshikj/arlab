2ceaa7841fc3934cad07775bb6e749a693d8f05a
woshikj <chevyckj@gmail.com>
2019-03-06 10:07:54 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -201,7 +201,7 @@ function init()
 				scanTextTimeout = setTimeout(function()
 				{
 					scanTextTimeout = null;
-					scanText.innerHTML = '<span style="left:50%;">Tap to continue</span>';
+					scanText.innerHTML = '<span style="position:absolute;left:0%;top:-10%;display:flex;justify-content: center;">Tap to continue</span>';
 				},2000);
 				scanText.innerHTML = '';
 			}
