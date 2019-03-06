886f46c7bab9985753c6caf6617609d43171678e
woshikj <chevyckj@gmail.com>
2019-03-06 10:01:20 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -325,14 +325,14 @@ function init()
 
 		scanText = document.createElement('div');
 		scanText.style.top = '80%';
-		scanText.style.left = '5%';
+		scanText.style.left = '10%';
 		scanText.style.position = 'absolute';
 		scanText.style.width = '100%';
 		scanText.style.height = '20%';
 		scanText.style.fontFamily = 'mont';
 		scanText.style.color = '#ffffff';
-		scanText.style.fontSize = '7vw';
-		scanText.innerHTML = "<img src=\"img/scan_qr.png\" style=\"position:absolute;top:-250%;left:5%;width:80%;\"/>Point Camera to QR Code";
+		scanText.style.fontSize = '6vw';
+		scanText.innerHTML = "<img src=\"img/scan_qr.png\" style=\"position:absolute;top:-250%;left:0%;width:80%;\"/>Point Camera to QR Code";
 
 		document.body.appendChild(scanText);
 	});
