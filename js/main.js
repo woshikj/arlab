8752a255568f2f14220be6ba1febfe61de6937e9
woshikj <chevyckj@gmail.com>
2019-03-07 17:44:45 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -353,9 +353,9 @@ function update()
 	textGroup.position.x = markerRoot1.position.x;
 	textGroup.position.y = markerRoot1.position.y - 0.05;
 	textGroup.position.z = markerRoot1.position.z;
-	textGroup.scale.x = markerRoot1.scale.x * 1.4;
-	textGroup.scale.y = markerRoot1.scale.y * 1.4;
-	textGroup.scale.z = markerRoot1.scale.z * 1.4;
+	textGroup.scale.x = markerRoot1.scale.x * 0.5;
+	textGroup.scale.y = markerRoot1.scale.y * 0.5;
+	textGroup.scale.z = markerRoot1.scale.z * 0.5;
 	textGroup.visible = markerRoot1.visible;
 	
 	for(var i = 0; i < textList.length; ++i)
